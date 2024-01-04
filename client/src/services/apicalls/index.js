8b00dc1a3297f92ba462a88a2400ee89bb787a44
Amansingh293 <106438123+Amansingh293@users.noreply.github.com>
2024-01-05 02:55:44 +0530

    Update index.js

--- client/src/services/apicalls/index.js
+++ client/src/services/apicalls/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://book-my-show-project-three.vercel.app/",
     withcredentials : true,
     headers : {
         'content-type' : 'application/json',
