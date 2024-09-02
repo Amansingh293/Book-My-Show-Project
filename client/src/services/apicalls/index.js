81a7b0dd7ef44b511f7828345ce2ad40732c2f68
Amansingh293 <amansinghdl9c@gmail.com>
2024-09-03 01:58:50 +0530

    Updated some glitches in UI and added more functionalities

--- client/src/services/apicalls/index.js
+++ client/src/services/apicalls/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  // baseURL: "https://book-my-show-project-three.vercel.app",
-  baseURL: "http://localhost:3001",
+  baseURL: "https://book-my-show-project-three.vercel.app",
+  // baseURL: "http://localhost:3001",
   withcredentials: true,
   headers: {
     "content-type": "application/json",
